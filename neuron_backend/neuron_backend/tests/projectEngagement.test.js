9d39176511efea9c3a41ef9b28fd199b10651601
Artvanq <artvan1947.hah@gmail.com>
2026-06-30 20:06:19 +0000

    аівавіаів

--- neuron_backend/neuron_backend/tests/projectEngagement.test.js
+++
@@ -1,8 +0,0 @@
-const test = require('node:test');
-const assert = require('node:assert/strict');
-const { normalizeTemplateName } = require('../utils/projectEngagement');
-
-test('normalizeTemplateName trims and caps length', () => {
-  assert.equal(normalizeTemplateName('  bug-report  '), 'bug-report');
-  assert.equal(normalizeTemplateName('x'.repeat(80)).length, 48);
-});
